7f2ccc225f98110db2b53694bd173cc1dc907d60
Joel Dean <jdeanjj1000@gmail.com>
2016-12-08 22:46:58 -0500

    added application module and application component classes for DI

---
+++ app/src/main/java/digify/tv/injection/component/ApplicationComponent.java
@@ -0,0 +1,8 @@
+package digify.tv.injection.component;
+
+/**
+ * Created by Joel on 12/8/2016.
+ */
+
+public class ApplicationComponent {
+}
---
+++ app/src/main/java/digify/tv/injection/module/ApplicationModule.java
@@ -0,0 +1,8 @@
+package digify.tv.injection.module;
+
+/**
+ * Created by Joel on 12/8/2016.
+ */
+
+public class ApplicationModule {
+}
