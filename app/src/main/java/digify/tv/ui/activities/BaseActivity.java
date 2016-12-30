ea432314b2497cc13b790061b89a71a0da38967b
Joel Dean <jdeanjj1000@gmail.com>
2016-12-30 01:43:50 -0500

    added login ui to display code and other pertinent information

--- app/src/main/java/digify/tv/ui/activities/BaseActivity.java
+++ app/src/main/java/digify/tv/ui/activities/BaseActivity.java
@@ -1,6 +1,7 @@
 package digify.tv.ui.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import javax.inject.Inject;
 import digify.tv.DigifyApp;
 import digify.tv.core.PreferenceManager;
 import digify.tv.injection.component.ApplicationComponent;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by Joel on 12/15/2016.
@@ -41,6 +43,11 @@ public class BaseActivity extends Activity {
 
     protected ApplicationComponent applicationComponent() {
         return DigifyApp.get(this).getComponent();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
 }
--- app/src/main/java/digify/tv/ui/activities/LoginActivity.java
+++ app/src/main/java/digify/tv/ui/activities/LoginActivity.java
@@ -1,31 +1,49 @@
 package digify.tv.ui.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.wang.avi.AVLoadingIndicatorView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import digify.tv.DigifyApp;
 import digify.tv.R;
 import digify.tv.api.DigifyApiService;
 import digify.tv.api.models.LoginResponseModel;
 import digify.tv.injection.component.ApplicationComponent;
-import digify.tv.util.*;
+import digify.tv.util.Utils;
 import eu.inloop.easygcm.EasyGcm;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends Activity {
 
     @Inject
     DigifyApiService digifyApiService;
+    @BindView(R.id.company_name)
+    TextView companyName;
+    @BindView(R.id.instruction)
+    TextView instruction;
+    @BindView(R.id.code)
+    TextView code;
+    @BindView(R.id.loading_view)
+    AVLoadingIndicatorView loadingView;
+    @BindView(R.id.activity_login)
+    RelativeLayout activityLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         applicationComponent().inject(this);
         EasyGcm.init(this);
@@ -33,17 +51,26 @@ public class LoginActivity extends Activity {
 
     }
 
-    public void login()
-    {
-        if(!Utils.isGooglePlayServicesAvailable(this))
+    public void login() {
+        loadingView.smoothToShow();
+        instruction.setText("Syncing device for first use");
+        code.setText("Please Wait");
+
+
+        if (!Utils.isGooglePlayServicesAvailable(this))
             return;
 
-        Call<LoginResponseModel> loginCall = digifyApiService.assignmentRequest(EasyGcm.getRegistrationId(this),Utils.getUniquePsuedoID());
+        Call<LoginResponseModel> loginCall = digifyApiService.assignmentRequest(EasyGcm.getRegistrationId(this), Utils.getUniquePsuedoID());
 
         loginCall.enqueue(new Callback<LoginResponseModel>() {
             @Override
             public void onResponse(Call<LoginResponseModel> call, Response<LoginResponseModel> response) {
-                Log.v("test",response.body().getCode());
+                if(response.body()!=null)
+                {
+                    instruction.setText("Enter this code into your dashboard");
+                    loadingView.smoothToHide();
+                    code.setText(response.body().getCode());
+                }
 
             }
 
@@ -55,6 +82,11 @@ public class LoginActivity extends Activity {
 
     protected ApplicationComponent applicationComponent() {
         return DigifyApp.get(this).getComponent();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
 
