4f86715ae1937de948edc036ebd9179b1b4f0a59
Joel Dean <jdeanjj1000@gmail.com>
2017-02-12 23:19:45 -0500

    added base layout for portrait content.

---
+++ app/src/main/java/digify/tv/PortraitActivity.java
@@ -0,0 +1,19 @@
+package digify.tv;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PortraitActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portrait);
+    }
+
+    public void generateSlider()
+    {
+        
+    }
+
+}
